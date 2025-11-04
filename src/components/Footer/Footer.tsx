967025249c437af3763a8a4ed97350fdee6ff976
Dmitrii Derendyaev <dds_stalker@mail.ru>
2025-11-05 00:00:22 +0400

    refactor: change address

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,12 +7,12 @@ const Footer = () => (
   <footer className={styles.footer}>
     <IonToolbar>
       <div className={styles.footerContent}>
-        <IonTitle className={styles.footerTitle}>Guangzhou Hone Machinery Co., Ltd.</IonTitle>
-        <p className={styles.description}>Ведущий китайский производитель машин для косметики и бытовой химии. Ваш эксперт по смесительным решениям.</p>
+        <IonTitle className={styles.footerTitle}>Brothers and Co., Ltd.</IonTitle>
+        <p className={styles.description}>Ведущий РОССИЙСКИЙ производитель машин для косметики и бытовой химии. Ваш эксперт по смесительным решениям.</p>
         <div className={styles.contactInfo}>
-          <p><strong>Адрес офиса:</strong> No. 119, Gaozeng Avenue, Renhe Town, Baiyun District, Guangzhou City, China</p>
-          <p><strong>Телефон:</strong> +86 18688638225</p>
-          <p><strong>Email:</strong> info@honemix.com</p>
+          <p><strong>Адрес офиса:</strong> Китайская Москва</p>
+          <p><strong>Телефон:</strong> +7 922 507-02-32</p>
+          <p><strong>Email:</strong> fortune.coffee.derendyaev.ru</p>
         </div>
         <p className={styles.rights}>© 2025 Guangzhou Hone Machinery Co., Ltd. Все права защищены</p>
         <IonButtons className={styles.socialButtons}>
